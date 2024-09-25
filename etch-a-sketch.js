67635b7533da209b20bdc06ce7ac7256fad9c1dd
aliang <ezerchen@hotmail.com>
2024-09-25 12:04:31 +0800

    add new feature add reset, random color, erase, shade, refactor create grid function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,62 @@
-function createDiv(count=60){
-    const container = document.querySelector(".container")
-    const divCount = count*count
-    for(let i=0;i<divCount;i++){
-        const div = document.createElement("div")
-        container.appendChild(div)
+const grid = document.querySelector(".container")
+
+function init(){
+    createGrid(80)
+    setColor('black')
+}
+
+init()
+
+function createGrid(count){
+    const cellCount = count*count
+    for(let i=0;i<cellCount;i++){
+        const cell = document.createElement("div")
+        cell.setAttribute("class", "cell")
+        cell.style.width=960/count +"px"
+        cell.style.height=cell.style.width
+        grid.appendChild(cell)
     }
 }
 
-createDiv()
+function setColor(color){
+    let gridCells = document.querySelectorAll(".cell")
+    for(let i=0; i<gridCells.length;i++){
+        gridCells[i].addEventListener("mouseenter", function(){
+            gridCells[i].style.backgroundColor=color
+        })
+    }
+}
+
+function clearGrid(){
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild)
+    }
+}
+
+function reset(){
+    clearGrid()
+    init()
+}
+
+function newGrid(){
+    let gridSize = prompt("Enter Number of Squares per Side: 1 - 100")
+    if(!Number.isInteger(+gridSize)||+gridSize>100 || +gridSize<1){
+        alert("Invalid Input, Please Enter a Integer between 1 and 100")
+        return
+    }
+    clearGrid()
+    createGrid(+gridSize)
+    setColor('black')
+}
+
+function eraseCell(){
+    let gridCells = document.querySelectorAll(".cell")
+    for(let i=0; i<gridCells.length;i++){
+        gridCells[i].addEventListener("mouseenter", function(){
+            gridCells[i].style.backgroundColor=null
+        })
+    }
+}
 
 function getRandomRGB(){
     const randomBetween = (min, max) => min + Math.floor(Math.random()*(max-min+1))
@@ -16,25 +65,19 @@ function getRandomRGB(){
     const b = randomBetween(0,255)
     return `rgb(${r},${g},${b})`
 }
-let interactionCount=1
-function generateSquares(){
-    const count = prompt("Enter Number of Squares per Side: 1 - 100")
-    if(!Number.isInteger(+count)||+count>100 || +count<1){
-        alert("Invalid Input, Please Enter a Integer between 1 and 100")
-        return
-    }
-    const container = document.querySelector(".container")
-    while(container.firstChild){
-        container.removeChild(container.firstChild)
-    }
-    container.style.opacity = 0.1*interactionCount
-    container.style.width = 16*count+"px"
-    container.style.height= 16*count+"px"
-    container.style["background-color"]=getRandomRGB()
-    createDiv(count)
-    if(interactionCount<10){
-        interactionCount++
-    }else{
-        interactionCount=1
+
+function setRandom(){
+    const rgb = getRandomRGB()
+    setColor(rgb)
+}
+
+function setShade(){
+    let gridCells = document.querySelectorAll(".cell")
+    for(let i=0; i<gridCells.length;i++){
+        gridCells[i].addEventListener("mouseenter", function(){
+            let j=0
+            if(j<10){j++}
+            gridCells[i].style.opacity= Number(gridCells[i].style.opacity)+0.1
+        })
     }
 }
